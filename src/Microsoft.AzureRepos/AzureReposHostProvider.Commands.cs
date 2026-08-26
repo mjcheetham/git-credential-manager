d11b311c45b049156e0a43e6ecee8bbce49cdf56
Matthew John Cheetham <mjcheetham@outlook.com>
2026-08-26 20:35:58 +0100

    azrepos: stub new account binding commands
    Signed-off-by: Matthew John Cheetham <mjcheetham@outlook.com>

--- src/Microsoft.AzureRepos/AzureReposHostProvider.Commands.cs
+++ src/Microsoft.AzureRepos/AzureReposHostProvider.Commands.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.CommandLine;
-using System.Linq;
-using System.Threading.Tasks;
 using GitCredentialManager;
 using GitCredentialManager.Commands;
 
@@ -15,211 +11,125 @@ public partial class AzureReposHostProvider
         //
         // clear-cache
         //
-        var clearCacheCmd = new Command("clear-cache", "Clear the Azure authority cache");
+        var clearCacheCmd = new Command("clear-cache", "Clear the Azure DevOps authority cache");
         clearCacheCmd.SetHandler(ClearCacheCmd);
 
         //
-        // list <organization> [--show-remotes] [--verbose]
+        // list [--org <org> | --tenant <tenant>]
+        //     [--local | --global]
         //
-        var listCmd = new Command("list", "List all user account bindings");
-        var orgFilterArg =
-            new Argument<string>("organization", "(optional) Filter results by Azure DevOps organization name")
-            {
-                Arity = ArgumentArity.ZeroOrOne
-            };
-        var remoteOpt = new Option<bool>("--show-remotes")
-        {
-            Description = "Also show Azure DevOps remote user bindings for the current repository"
-        };
-        var verboseOpt = new Option<bool>(new[] { "--verbose", "-v" }, "Verbose output - show remote URLs");
-        listCmd.AddArgument(orgFilterArg);
-        listCmd.AddOption(remoteOpt);
-        listCmd.AddOption(verboseOpt);
-        listCmd.SetHandler(ListCmd, orgFilterArg, remoteOpt, verboseOpt);
+        var listCmd = new Command("list", "List all Microsoft Entra accounts in the cache.");
+        var listTenantOpt = new Option<string>(["--tenant", "-t"], "Tenant ID or name to filter results to. (optional)");
+        var listOrgOpt = new Option<string>(["--org", "-o"], "Azure DevOps organization name to filter results to. (optional)");
+        listCmd.AddOptionSet(OptionArity.ZeroOrOne, listTenantOpt, listOrgOpt);
+        var listGlobalOpt = new Option<bool>("--global", "List accounts for all repositories. (default)");
+        var listLocalOpt = new Option<bool>("--local", "List accounts for the current repository only.");
+        listCmd.AddOptionSet(OptionArity.ZeroOrOne, listGlobalOpt, listLocalOpt);
+        listCmd.SetHandler(ListCmd, listTenantOpt, listOrgOpt, listGlobalOpt, listLocalOpt);
 
         //
-        // bind <organization> <username> [--local]
+        // login [--org <org> | --tenant <tenant>]
+        //     [--local | --global]
         //
-        var bindCmd = new Command("bind", "Bind a user account to an Azure DevOps organization");
-        var orgArg = new Argument<string>("organization", "Azure DevOps organization name")
-        {
-            Arity = ArgumentArity.ExactlyOne
-        };
-        var userNameArg = new Argument<string>("username", "Username or email (e.g.: alice@example.com)")
-        {
-            Arity = ArgumentArity.ExactlyOne
-        };
-        var localOpt = new Option<bool>("--local", "Target the local repository Git configuration");
-        bindCmd.AddArgument(orgArg);
-        bindCmd.AddArgument(userNameArg);
-        bindCmd.AddOption(localOpt);
-        bindCmd.SetHandler(BindCmd, orgArg, userNameArg, localOpt);
+        var loginCmd = new Command("login", "Sign in to a Microsoft Entra account.");
+        var loginTenantOpt = new Option<string>(["--tenant", "-t"], "Tenant ID or name to use with this account.");
+        var loginOrgOpt = new Option<string>(["--org", "-o"], "Azure DevOps organization name to use with this account.");
+        loginCmd.AddOptionSet(OptionArity.ZeroOrOne, loginTenantOpt, loginOrgOpt);
+        var loginGlobalOpt = new Option<bool>("--global", "Sign in for all repositories. (default)");
+        var loginLocalOpt = new Option<bool>("--local", "Sign in for the current repository only.");
+        loginCmd.AddOptionSet(OptionArity.ZeroOrOne, loginGlobalOpt, loginLocalOpt);
+        loginCmd.SetHandler(LoginCmd, loginTenantOpt, loginOrgOpt, loginGlobalOpt, loginLocalOpt);
 
         //
-        // unbind <organization> [--local]
+        // logout --tenant <tenant> (--id <account-id> | --username <username>)
+        //     [--local | --global]
+        // logout --org <org> (--id <account-id> | --username <username>)
+        //     [--local | --global]
         //
-        var unbindCmd = new Command("unbind")
-        {
-            Description = "Remove user account binding for an Azure DevOps organization",
-        };
-        unbindCmd.AddArgument(orgArg);
-        unbindCmd.AddOption(localOpt);
-        unbindCmd.SetHandler(UnbindCmd, orgArg, localOpt);
+        var logoutCmd = new Command("logout", "Sign out of a Microsoft Entra account.");
+        var logoutIdOpt = new Option<string>("--id", "Account ID to sign out.");
+        var logoutUserNameOpt = new Option<string>(["--username", "-u"], "Username (UPN) of the account to sign out.");
+        var logoutOrgOpt = new Option<string>(["--org", "-o"], "Azure DevOps organization name to sign out from.");
+        var logoutTenantOpt = new Option<string>(["--tenant", "-t"], "Tenant ID or name to sign out from.");
+        var logoutGlobalOpt = new Option<bool>("--global", "Sign out for all repositories. (default)");
+        var logoutLocalOpt = new Option<bool>("--local", "Sign out for the current repository only.");
+        logoutCmd.AddOptionSet(OptionArity.ExactlyOne, logoutIdOpt, logoutUserNameOpt);
+        logoutCmd.AddOptionSet(OptionArity.ExactlyOne, logoutOrgOpt, logoutTenantOpt);
+        logoutCmd.AddOptionSet(OptionArity.ZeroOrOne, logoutGlobalOpt, logoutLocalOpt);
+        logoutCmd.SetHandler(
+            LogoutCmd, logoutIdOpt, logoutUserNameOpt, logoutOrgOpt, logoutTenantOpt,
+            logoutGlobalOpt, logoutLocalOpt);
+
+        //
+        // set --tenant <tenant>  (--id <account-id> | --username <username>)
+        //     [--local | --global]
+        // set --org <org> (--id <account-id> | --username <username>)
+        //     [--local | --global]
+        //
+        var setCmd = new Command("set", "Set which Microsoft Entra account should be used for Azure Repos authentication.");
+        var setIdOpt = new Option<string>("--id", "Account ID to use.");
+        var setUserNameOpt = new Option<string>(["--username", "-u"], "Username (UPN) of the account to use.");
+        var setOrgOpt = new Option<string>(["--org", "-o"], "Azure DevOps organization name to use an account with.");
+        var setTenantOpt = new Option<string>(["--tenant", "-t"], "Tenant ID or name to use an account with.");
+        var setGlobalOpt = new Option<bool>("--global", "Set the account for all repositories. (default)");
+        var setLocalOpt = new Option<bool>("--local", "Set the account for the current repository only.");
+        setCmd.AddOptionSet(OptionArity.ExactlyOne, setIdOpt, setUserNameOpt);
+        setCmd.AddOptionSet(OptionArity.ExactlyOne, setOrgOpt, setTenantOpt);
+        setCmd.AddOptionSet(OptionArity.ZeroOrOne, setGlobalOpt, setLocalOpt);
+        setCmd.SetHandler(
+            SetCmd, setIdOpt, setUserNameOpt, setOrgOpt, setTenantOpt,
+            setGlobalOpt, setLocalOpt);
+
+        //
+        // unset --tenant <tenant>
+        //     [--local | --global]
+        // unset --org <org>
+        //     [--local | --global]
+        //
+        var unsetCmd = new Command("unset", "Forget a Microsoft Entra account for Azure Repos authentication.");
+        var unsetOrgOpt = new Option<string>(["--org", "-o"], "Azure DevOps organization name to forget an account with.");
+        var unsetTenantOpt = new Option<string>(["--tenant", "-t"], "Tenant ID or name to forget an account with.");
+        var unsetGlobalOpt = new Option<bool>("--global", "Forget the account for all repositories. (default)");
+        var unsetLocalOpt = new Option<bool>("--local", "Forget the account for the current repository only.");
+        unsetCmd.AddOptionSet(OptionArity.ExactlyOne, unsetOrgOpt, unsetTenantOpt);
+        unsetCmd.AddOptionSet(OptionArity.ZeroOrOne, unsetGlobalOpt, unsetLocalOpt);
+        unsetCmd.SetHandler(UnsetCmd, unsetOrgOpt, unsetTenantOpt, unsetGlobalOpt, unsetLocalOpt);
 
         var rootCmd = new ProviderCommand(this);
         rootCmd.AddAlias("azrepos");
         rootCmd.AddAlias("ado");
-        rootCmd.AddCommand(listCmd);
-        rootCmd.AddCommand(bindCmd);
-        rootCmd.AddCommand(unbindCmd);
         rootCmd.AddCommand(clearCacheCmd);
+        rootCmd.AddCommand(listCmd);
+        rootCmd.AddCommand(loginCmd);
+        rootCmd.AddCommand(logoutCmd);
+        rootCmd.AddCommand(setCmd);
+        rootCmd.AddCommand(unsetCmd);
         return rootCmd;
+    }
+
+    private void ListCmd(string tenant, string org, bool global, bool local)
+    {
+    }
+
+    private void LoginCmd(string tenant, string org, bool global, bool local)
+    {
+    }
+
+    private void LogoutCmd(string accountId, string userName, string org, string tenant, bool global, bool local)
+    {
+    }
+
+    private void SetCmd(string accountId, string userName, string org, string tenant, bool global, bool local)
+    {
+    }
+
+    private void UnsetCmd(string org, string tenant, bool global, bool local)
+    {
     }
 
     private void ClearCacheCmd()
     {
         _authorityCache.Clear();
         _context.Console.WriteLine("Authority cache cleared");
-    }
-
-    private class RemoteBinding
-    {
-        public string Remote { get; set; }
-        public bool IsPush { get; set; }
-        public Uri Uri { get; set; }
-    }
-
-    private void ListCmd(string organization, bool showRemotes, bool verbose)
-    {
-        // Get all organization bindings from the user manager
-        IList<AzureReposBinding> bindings = _bindingManager.GetBindings(organization).ToList();
-        IDictionary<string, IEnumerable<AzureReposBinding>> orgBindingMap =
-            bindings.GroupBy(x => x.Organization).ToDictionary();
-
-        // If we are asked to also show remotes we build the remote binding map
-        var orgRemotesMap = new Dictionary<string, ICollection<RemoteBinding>>();
-        if (showRemotes)
-        {
-            if (!_context.Git.IsInsideRepository())
-            {
-                _context.Console.WriteWarning("not inside a git repository (--show-remotes has no effect)");
-            }
-
-            static bool IsAzureDevOpsHttpRemote(string url, out Uri uri)
-            {
-                return Uri.TryCreate(url, UriKind.Absolute, out uri) &&
-                       (StringComparer.OrdinalIgnoreCase.Equals(Uri.UriSchemeHttp, uri.Scheme) ||
-                        StringComparer.OrdinalIgnoreCase.Equals(Uri.UriSchemeHttps, uri.Scheme)) &&
-                       UriHelpers.IsAzureDevOpsHost(uri.Host);
-            }
-
-            foreach (GitRemote remote in _context.Git.GetRemotes())
-            {
-                if (IsAzureDevOpsHttpRemote(remote.FetchUrl, out Uri fetchUri))
-                {
-                    string fetchOrg = UriHelpers.GetOrganizationName(fetchUri);
-                    var binding = new RemoteBinding { IsPush = false, Remote = remote.Name, Uri = fetchUri };
-                    orgRemotesMap.Append(fetchOrg, binding);
-                }
-
-                if (IsAzureDevOpsHttpRemote(remote.PushUrl, out Uri pushUri))
-                {
-                    string pushOrg = UriHelpers.GetOrganizationName(pushUri);
-                    var binding = new RemoteBinding { IsPush = true, Remote = remote.Name, Uri = pushUri };
-                    orgRemotesMap.Append(pushOrg, binding);
-                }
-            }
-        }
-
-        bool isFiltered = !string.IsNullOrWhiteSpace(organization);
-        string indent = isFiltered ? string.Empty : "  ";
-
-        // Get the set of all organization names (organization names are not case sensitive)
-        ISet<string> orgNames = new HashSet<string>(orgBindingMap.Keys, StringComparer.OrdinalIgnoreCase);
-        orgNames.UnionWith(orgRemotesMap.Keys);
-
-        var icmp = StringComparer.OrdinalIgnoreCase;
-
-        foreach (string orgName in orgNames)
-        {
-            if (!isFiltered)
-            {
-                _context.Console.WriteLine($"{orgName}:");
-            }
-
-            // Print organization bindings
-            foreach (AzureReposBinding binding in orgBindingMap.GetValues(orgName))
-            {
-                if (binding.GlobalUserName != null)
-                {
-                    _context.Console.WriteLine($"{indent}(global) -> {binding.GlobalUserName}");
-                }
-
-                if (binding.LocalUserName != null)
-                {
-                    string value = string.IsNullOrEmpty(binding.LocalUserName)
-                        ? "(no inherit)"
-                        : binding.LocalUserName;
-                    _context.Console.WriteLine($"{indent}(local)  -> {value}");
-                }
-            }
-
-            // Print remote bindings
-            IEnumerable<IGrouping<string, RemoteBinding>> remoteBindingMap =
-                orgRemotesMap.GetValues(orgName).GroupBy(x => x.Remote);
-
-            foreach (var remoteBinding in remoteBindingMap)
-            {
-                _context.Console.WriteLine($"{indent}{remoteBinding.Key}:");
-                foreach (RemoteBinding binding in remoteBinding)
-                {
-                    // User names in dev.azure.com URLs cannot always be used as *actual user names*
-                    // because of the unfortunate decision to use this field to get the Azure DevOps
-                    // organization name to be sent by Git to credential helpers.
-                    //
-                    // We show dev.azure.com URLs as "inherit", if there is a username that matches
-                    // the organization name.
-                    if (!binding.Uri.TryGetUserInfo(out string userName, out _) ||
-                        UriHelpers.IsDevAzureComHost(binding.Uri.Host) && icmp.Equals(userName, orgName))
-                    {
-                        userName = "(inherit)";
-                    }
-
-                    string url = null;
-                    if (verbose)
-                    {
-                        url = $"{binding.Uri.WithoutUserInfo()} ";
-                    }
-
-                    _context.Console.WriteLine(binding.IsPush
-                        ? $"{indent}  {url}(push)  -> {userName}"
-                        : $"{indent}  {url}(fetch) -> {userName}");
-                }
-            }
-        }
-    }
-
-    private Task<int> BindCmd(string organization, string userName, bool local)
-    {
-        if (local && !_context.Git.IsInsideRepository())
-        {
-            _context.Console.WriteError("not inside a git repository (cannot use --local)");
-            return Task.FromResult(-1);
-        }
-
-        _bindingManager.Bind(organization, userName, local);
-        return Task.FromResult(0);
-    }
-
-    private Task<int> UnbindCmd(string organization, bool local)
-    {
-        if (local && !_context.Git.IsInsideRepository())
-        {
-            _context.Console.WriteError("not inside a git repository (cannot use --local)");
-            return Task.FromResult(-1);
-        }
-
-        _bindingManager.Unbind(organization, local);
-        return Task.FromResult(0);
     }
 }
